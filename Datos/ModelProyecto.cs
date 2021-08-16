c48f5119e0a2da176f74072c9c664cd047e4f5c2
alphazkr <alphazkr@users.noreply.github.com>
2021-08-15 22:39:37 -0500

    Se realiza BackUp Para ver si funciona en el GIT

---
+++ Datos/ModelProyecto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Datos
+{
+    public partial class ModelProyecto : DbContext
+    {
+        public ModelProyecto()
+            : base("name=ProyectoContext")
+        {
+        }
+
+        public virtual DbSet<TblUbicacion> TblUbicacion { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblUbicacion>()
+                .Property(e => e.cPais)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<TblUbicacion>()
+                .Property(e => e.cCiudad)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<TblUbicacion>()
+                .Property(e => e.cTipoAvistamiento)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<TblUbicacion>()
+                .Property(e => e.cDescripción)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<TblUbicacion>()
+                .Property(e => e.cEstatus)
+                .IsUnicode(false);
+        }
+    }
+}
